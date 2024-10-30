5858382661bf6274b7f22666443ccf5dea647aba
Lorenzo Hu <hu.lorenzo@iisgalvanimi.edu.it>
2024-10-30 09:20:46 +0000

    fatto punto E

---
+++ prep_E.py
@@ -0,0 +1,16 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+   database="Animali"
+)
+mycursor = mydb.cursor()
+
+def visualizza_animali_peso_2():
+    mycursor.execute("SELECT * FROM Mammiferi WHERE Peso > 2")
+    result = mycursor.fetchall()
+    for animali in result:
+        print(animali)
+
+visualizza_animali_peso_2()
